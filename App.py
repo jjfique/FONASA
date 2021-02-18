61071c7bec9cab3eb09e77b6f114d36cc3d4a247
JONATHAN FIQUE <jjfique@gmail.com>
2021-02-18 17:06:40 -0500

    ajuste para flask

---
+++ App.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysql_connector import MySQL
+
+# initializations
+app = Flask(__name__)
+
+# Mysql Connection
+app.config['MYSQL_HOST'] = 'localhost' 
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DATABASE'] = 'fonasa'
+mysql = MySQL(app)
+
+# settings
+app.secret_key = "mysecretkey"
+
+# routes
+@app.route('/')
+def Index():
+    return render_template('index.html')
+
+
+
+
+
+# starting the app
+if __name__ == "__main__":
+    app.run(port=3000, debug=True)
