343753dd39eed0705cabe7617f37bdabd575067d
JONATHAN FIQUE <jjfique@gmail.com>
2021-02-19 16:44:31 -0500

    envio de label

--- App.py
+++ App.py
@@ -18,8 +18,25 @@ app.secret_key = "mysecretkey"
 @app.route('/')
 def Index():
     return render_template('index.html')
+@app.route('/RegistroPaciente')
+def RegistroPaciente():
+    return render_template('RegistroPaciente.html')
 
-
+@app.route('/Registro_Paciente', methods=['GET','POST'])
+def Registro_Paciente():
+    if request.method == 'POST':
+        Nombre = request.form['txt_Nombre']
+        PesoEstatura = request.form.values['list_PesoEstatura']
+     #   Fuma = request.form['check_Fuma']
+        FechaNacimiento = request.form['date_FechaNacimiento']
+        TiempoFumando = request.form['Txt_TiempoFumando']
+      #  Dieta = request.form['check_Dieta']
+        print(Nombre)
+        print(PesoEstatura)
+        print(FechaNacimiento)
+        print(TiempoFumando)
+    return  "LISTO"
+    
 
 
 
